c60d4c88aac62385ef1fd3707d3d6469a63a22c6
Lori Boyd <lori.p.boyd@gmail.com>
2020-05-20 16:25:26 -0400

    adds a update function to tips

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,4 +1,5 @@
 class TipsController < ApplicationController
+  before_action :set_tip, only: [ :update, :destroy ]
 
   def index
     @tips = Tip.all
@@ -13,17 +14,28 @@ class TipsController < ApplicationController
     render json: @tip
   end
 
+  def update
+    @tip.update(tip_params)
+
+    render json: @tip
+  end
+
   def destroy
-    @tip = Tip.find(params[:id])
+    @tip.destroy
 
     render json: { 'status': 'ok' }
   end
 
   private
 
+  # DRY
+  def set_tip
+    @tip = Tip.find(params[:id])
+  end
+
   # strong params
   def tip_params
-    params.permit(:tip_percent, :total, :fees, :user_id)
+    params.permit(:tip_percent, :total, :fees, :user_id, :service_rating, :exp_rating)
   end
 
 end
